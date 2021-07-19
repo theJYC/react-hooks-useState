21c20e32c03b10d9e932eaedeb14cc07e1cc338f
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-07-19 14:25:04 -0400

    counter initialised

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
 function App() {
+  //destructuring hook, into [value, f(x)]:
+  //n.b. useState(0) returns an array with the value stored (i.e. count),
+  //and the method to manipulate the value (i.e. setCount)
+  const [count, setCount] = useState(0)
+
+  function increment() {
+    console.log(`${count} incremented to ${count + 1}`)
+    setCount(prevCount => prevCount + 1)
+  }
+
+  function decrement() {
+    console.log(`${count} incremented to ${count - 1}`)
+    setCount(prevCount => prevCount - 1)
+  }
+
+  function double() {
+    setCount(prevCount => prevCount * 2)
+  }
+
+  function halve() {
+    setCount(prevCount => prevCount / 2)
+  }
+
+  function reset() {
+    setCount(0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>{count}</h1>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={double}>Double</button>
+        <button onClick={halve}>Halve</button>
+        <button onClick={reset}>Reset</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
